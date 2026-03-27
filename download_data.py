7591e507d3ef7ea81d5cefa24da1d0a073c94cec
ajgelpi <103214850+ajgelpi@users.noreply.github.com>
2026-03-26 21:09:18 -0500

    updated requirements and changed download_data path to match data_preprocessing

--- download_data.py
+++ download_data.py
@@ -1,8 +1,8 @@
 import kaggle
 import os
 
-os.makedirs("data", exist_ok=True)
+os.makedirs("data_raw", exist_ok=True)
 
 dataset_id = "masoudnickparvar/brain-tumor-mri-dataset"
 
-kaggle.api.dataset_download_files(dataset_id, path="./data", unzip=True)
+kaggle.api.dataset_download_files(dataset_id, path="data_raw", unzip=True)
