55f886a88c82aedd6fdcc31bc381859912cc007e
ajgelpi <103214850+ajgelpi@users.noreply.github.com>
2026-03-26 21:12:29 -0500

    added comments to download_data

--- download_data.py
+++ download_data.py
@@ -1,8 +1,17 @@
+"""
+Must create kaggle.json file in
+- Windows - C:\Users\<Windows-username>\.kaggle\kaggle.json
+- Mac/Linux - ~/.kaggle/kaggle.json
+"""
+
 import kaggle
 import os
 
+# Make directory for raw data
 os.makedirs("data_raw", exist_ok=True)
 
+# Kaggle dataset path
 dataset_id = "masoudnickparvar/brain-tumor-mri-dataset"
 
+# Download using kaggle api
 kaggle.api.dataset_download_files(dataset_id, path="data_raw", unzip=True)
