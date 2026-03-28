d79df3c711bd3ade3a019c8d1978c73e26c542af
ajgelpi <103214850+ajgelpi@users.noreply.github.com>
2026-03-27 19:33:02 -0500

    created starter code for the vgg model

---
+++ models/vgg19.py
@@ -0,0 +1,47 @@
+import torchvision
+from torch import nn
+import torch
+
+# VGG Model Class
+class VGGModel(nn.Module):
+    def __init__(self, pretrained=True, batch_norm = True, freeze = True, self_attention=False):
+        super().__init__()
+
+        # download vgg model
+        if batch_norm:
+            model = torchvision.models.vgg19_bn
+            weights = torchvision.models.VGG19_BN_Weights.DEFAULT
+        else:
+            model = torchvision.models.vgg19
+            weights = torchvision.models.VGG19_Weights.DEFAULT
+
+        # get weights for vgg model
+        weights = weights if pretrained else None
+
+        # initialize vgg model
+        self.vgg = model(weights=weights)
+
+        # freeze parameter updates
+        if freeze: 
+            for param in self.vgg.parameters():
+                param.requires_grad = False
+
+        # replace final layer with 4 classes
+        final_in_features = self.vgg.classifier[-1].in_features
+        self.vgg.classifier[-1] = nn.Linear(final_in_features, 4)
+
+        # replace last 2 conv blocks with self-attention blocks
+        if self_attention:
+            pass
+    
+    def forward(self, x):
+        return self.vgg(x)
+    
+
+class SABlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
