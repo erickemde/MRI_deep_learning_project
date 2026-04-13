30d8e43e53cf58e7b1b446dc28b3c4f4ad8dc581
ajgelpi <103214850+ajgelpi@users.noreply.github.com>
2026-04-13 16:17:44 -0500

    Update lit_vgg.py
    added comments

--- src/models/lit_vgg.py
+++ src/models/lit_vgg.py
@@ -5,7 +5,11 @@ from torch import nn
 from torchmetrics import Accuracy
 
 class LitVGG(L.LightningModule):
-    def __init__(self, model = VGGModel(), optimizer = optim.Adam, scheduler = optim.lr_scheduler.OneCycleLR, lr=1e-3):
+    def __init__(self, 
+                 model = VGGModel(), 
+                 optimizer = optim.Adam, 
+                 scheduler = optim.lr_scheduler.OneCycleLR, 
+                 lr=1e-3):
         super().__init__()
         self.save_hyperparameters(ignore=["model"])
         self.model = model
@@ -20,24 +24,36 @@ class LitVGG(L.LightningModule):
         x, y = batch
         logits = self.model(x)
         loss = self.loss_fn(logits, y)
+        
+        # accuracy
         preds = logits.argmax(dim=1)
         self.train_acc(preds, y)
+        
+        # logging
         self.log("train/train_loss", loss)
         self.log("train/train_acc", self.train_acc, on_step=False, on_epoch=True)
+        
         return loss
     
     def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self.model(x)
         loss = self.loss_fn(logits, y)
+        
+        # accuracy
         preds = logits.argmax(dim=1)
         self.val_acc(preds, y)
+
+        # logging
         self.log("valid/valid_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("valid/valid_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+
         return loss
     
     def configure_optimizers(self):
+        # create optimizer
         optimizer = self.optimizer(self.parameters(), lr=self.lr)
+        # create scheduler
         scheduler = self.scheduler(
             optimizer,
             max_lr=self.lr,
